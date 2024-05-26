b7d16f53a21bbb357aefcd8c30f72d0273b110b5
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-26 01:01:47 -0500

    Implementando un for para crear el encabezado de la table, salen errores

--- pub/programa.js
+++ pub/programa.js
@@ -4,6 +4,21 @@ function movies(fecha){
     year: parseInt(fecha)
   }
   console.log(data);
+  fech(url)
+    .then(response => response.json())
+    .the(data => {
+      const contenedorTabla = document.querySelector('#table');
+      const tabla = document.createElement('table');
+      const encabezado = tabla.createTHead();
+      const filaE = tabla.insertRow();
+      
+      for (const key in datos[0]){
+        const th = document.createElement('th');
+        th.textContent = key.toUpperCase();
+        filaE.appendChild(th);
+      }
+      contenedorTabla.appendChild(tabla);
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function(){
