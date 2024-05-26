b5f5e24491151cd4f4a8684f017e5901ccda3af9
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-26 01:28:42 -0500

    Haciendo una prueba, de esta manera si se imprime en el navegador

--- index.js
+++ index.js
@@ -33,9 +33,8 @@ conexion.connect((err) => {
   }
   
   app.get('/basedata',(request, response) => {
-    const fecha = request.body.year;
-    console.log(fecha);
-    conexion.query('SELECT * FROM movie WHERE Year = ?',[fecha],(err,filas) => {
+    //const fecha = request.body.year;
+    conexion.query('SELECT * FROM movie WHERE Year = 1977',(err,filas) => {
       if (err) {
         console.error('Error en la consulta')
         response.status(500).json({error: 'Error en la consulta'});
