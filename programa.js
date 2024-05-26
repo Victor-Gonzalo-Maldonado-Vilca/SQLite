ad0f5a263f624d874e9c2c9d20e845caf251dd7e
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-25 23:50:28 -0500

    Agregando id de formulario, para poder acceder al submit correspondiente

--- programa.js
+++ programa.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const fecha = document.querySelector('#year');
+  document.querySelector('#formulario').onsubmit = (event) => {
+    event.preventDefault();
+  }
 });
