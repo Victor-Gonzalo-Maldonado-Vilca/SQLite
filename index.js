9695a8841eff1a73dca6b71bdde0f32afab4bed1
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-26 00:22:18 -0500

    Agregando directorio pub para una mejor organización

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const express = require('express');
+const bp = require('body-parser');
 const mysql = require('mysql')
 const app = express();
+
+app.use(express.static('pub'));
+app.use(bp.json);
+app.use(bp.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000")
