52c761bc97e1844740dac2c5b3e505f324afdba0
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-26 00:03:20 -0500

    Agregando funcion fecha(), no se imprimen los datos en el console.log de programa.js

--- programa.js
+++ programa.js
@@ -1,6 +1,16 @@
+function (fecha){
+  const url = 'http://localhost:3000/basedata';
+  const data = {
+    year: fecha
+  }
+  console.log(data);
+}
+
 document.addEventListener('DOMContentLoaded', function(){
   const fecha = document.querySelector('#year');
   document.querySelector('#formulario').onsubmit = (event) => {
     event.preventDefault();
+    movies(fecha);
+    return false;
   }
 });
