d9bc77f3d355a6ca841097016665b64fb3c3620a
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-25 22:55:17 -0500

    Estableciendo conexion Mysql, salen errores de conexion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const mysql = require('mysql')
 const app = express();
 
 app.listen(3000, () => {
@@ -9,4 +10,20 @@ app.listen(3000, () => {
 
 app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'));
+});
+const conexion = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'Victor',
+  password: 'maldonado100204.',
+  database: 'peliculas'
+});
+
+conexion.connect((err) => {
+  if (err) {
+    console.error('Error de conexion mysql ', err);
+    return;
+  } else {
+    console.log('Conexion Establecida')
+  }
 });
